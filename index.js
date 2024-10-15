55a3203d727ee5e577bbfee02fca9e639bdadc75
MoaufmKlo <45636897+MoaufmKlo@users.noreply.github.com>
2024-10-15 11:38:30 +0200

    feat: add buildings, cars, states and trailers

---
+++ index.js
@@ -0,0 +1,11 @@
+import buildings from './data/buildings.json' assert { type: 'json' }
+import cars from './data/cars.json' assert { type: 'json' }
+import states from './data/states.json' assert { type: 'json' }
+import trailers from './data/trailers.json' assert { type: 'json' }
+
+export default {
+  buildings,
+  cars,
+  states,
+  trailers
+}
